c5ffbf5e040cf959ccb5a6ed572eccc6cf0cf314
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-09 18:37:45 +0530

    Add some important text contents.

--- src/Node.java
+++ src/Node.java
@@ -5,4 +5,7 @@
  */
 
 public class Node {
+    // A node in data structures is like a box containing two things -->
+    // 1. A value of any data type which you want to store in it.
+    // 2. The address of the next node to which it is referring i.e. The address of the node which comes just after it.
 }
