c4d746bceed89c50058d57ac2714114f7dd16534
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-09 18:50:36 +0530

    Add some important text info as comment.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -10,6 +10,6 @@ public class LinkedList {
     Node head;
 
     public void insert(int data) {
-        Node node = new Node();
+        Node node = new Node(); // Every time data is inserted, it will be inserted in a new node.
     }
 }
