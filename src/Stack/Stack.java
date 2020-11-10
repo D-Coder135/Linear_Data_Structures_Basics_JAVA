2671d53a1b0c55563e29cd901c6fdf1a4b500b0c
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-10 23:45:37 +0530

    Add some important text info as comment.

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -6,6 +6,13 @@
 package Stack;
 
 public class Stack {
+    // A Stack is a arrangement in Data Structures which follows LIFO mechanism i.e Last In First Out!
+    // Adding values/data to the stack is known as push.
+    // Fetching values/data from the stack is known as pop.
+    // We can also search values/data from a stack, which is known as peek.
+    // Hence LIFO mechanism means we can only pull the last pushed element in a stack.
+
+    // The last value/data which has been pushed in a stack in considered as top.
 
     public void push(int data) {
 
