874ada76da5a97f899ea66834ffab57c4fd63f7b
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-26 22:27:52 +0530

    Add some important text description as comment about Queue.

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -6,4 +6,11 @@
 package Queue;
 
 public class Queue {
+    // Queue in Data structures follow FIFO: First In First Out.
+    // Queue can be implemented in JAVA using --> 1. Arrays & 2. LinkedList
+    // Here, Queue is implemented using arrays.
+
+
+    // In Queue, when we add something then that process is known as enqueue. (Just like push in stack)
+    // And when we delete or remove values from it, then that is known as dequeue. (Just like pop in stack)
 }
