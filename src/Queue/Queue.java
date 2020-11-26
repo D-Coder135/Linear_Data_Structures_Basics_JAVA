b18dc2daacc0417ffc838603ca2bfe8bc120cff0
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-26 22:45:21 +0530

    Add some more important text info as description in the comment

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -7,6 +7,13 @@ package Queue;
 
 public class Queue {
     // Queue in Data structures follow FIFO: First In First Out.
+    // In a Queue there are two ends -->
+    // First end is from where the values will be inserted. And Second end is from where the values will be removed.
+
+    // There are two main terms related to a Queue in Data Structures -->
+    // 1. Front(Represents the Front End of the Queue)
+    // 2. Rear(Represents the back end of the Queue)
+
     // Queue can be implemented in JAVA using --> 1. Arrays & 2. LinkedList
     // Here, Queue is implemented using arrays.
 
@@ -16,5 +23,6 @@ public class Queue {
 
     int[] queue = new int[5];
     int size; // To store the size of the queue.
+
 
 }
